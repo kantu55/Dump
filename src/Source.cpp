0d6f84efda899691bf7b58c22593f7f71b53678b
kantu55 <49814798+kantu55@users.noreply.github.com>
2021-04-07 22:55:58 +0900

    fread_s使った

--- src/Source.cpp
+++ src/Source.cpp
@@ -1,19 +1,59 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+#define BUFFERSIZE 30
+#define DATASIZE 22
+#define ELEMENTCOUNT 2
+#define ELEMENTSIZE (DATASIZE/ELEMENTCOUNT)
+#define FILENAME "hello.txt"
+
 
 int main(void)
 {
 	FILE* file;
 	int data;
 	int count = 0;
+	unsigned char list[256];
+	
 
+	if (fopen_s(&file, FILENAME, "rb") == 0)
+	{
+		data = fread_s(list, 256, sizeof(unsigned char), sizeof(list) / sizeof(list[0]), file);
+		
+		for (int i = 0; i < data; ++i)
+		{
+			if (i == 0)
+			{
+				printf("%08x ", count);
+			}
+			else if (i % 16 == 0)
+			{
+				printf("\n");
+				count += 16;
+				printf("%08x ", count);
+			}
+			else if (i % 8 == 0)
+			{
+				printf(" ");
+			}
+
+			printf(" ");
+			printf("%02x", list[i]);
+		}
+		printf("読み込み終了");
+		
+		fclose(file);
+	}
+	/*
 	file = fopen("hello.txt", "rb");
 	if (!file)
 	{
 		printf("ファイルが開けません.\n");
 		return 0;
 	}
+
+
+
 
 	while (1)
 	{
@@ -38,7 +78,7 @@ int main(void)
 	}
 
 
-	fclose(file);
+	fclose(file);*/
 
 	return 0;
 }
